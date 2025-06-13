1c8dda17bf74739d5f00dfa4d09ef9b74691b8fa
JoelLuna02 <hectorjoelluna@outlook.com.ar>
2025-06-12 23:43:01 -0300

    Prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+	semi: true,
+	singleQuote: false,
+	useTabs: true,
+	trailingComma: "all",
+	endOfLine: "lf",
+	printWidth: 100,
+};
+
+export default config;
