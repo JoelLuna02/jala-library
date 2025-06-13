f658263faf3a25bfe8e9f15190847ffec5db5a51
JoelLuna02 <hectorjoelluna@outlook.com.ar>
2025-06-13 00:03:03 -0300

    Implement next themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
@@ -9,7 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
 	}, []);
 	return (
 		<SessionProvider session={pageProps.session}>
-			<Component {...pageProps} />
+			<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+				<Component {...pageProps} />
+			</ThemeProvider>
 		</SessionProvider>
 	);
 }
